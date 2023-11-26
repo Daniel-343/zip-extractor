184381458d066a569a1aa4b23c3fadfcd1165ff5
Daniel-343 <danthe34@gmail.com>
2023-11-26 13:31:12 +0100

    change database layout

--- create.sql
+++ create.sql
@@ -1,10 +1,19 @@
-CREATE TABLE IF NOT EXISTS "user"
-(
-    id serial,
-    name varchar(50),
-    password varchar(50)
+DROP TABLE IF EXISTS "user";
+
+CREATE TABLE  IF NOT EXISTS "user" (
+	id serial PRIMARY KEY,
+	name VARCHAR ( 50 ) NOT NULL,
+	password VARCHAR ( 50 ) NOT NULL,
+    company VARCHAR ( 50 ),
+	customer_code INTEGER NOT NULL,
+    type VARCHAR (50) NOT NULL
 );
-INSERT INTO "user" (name, password) VALUES
-('John Doe', 'password123'),
-('Jane Smith', 'pass444'),
-('Bob Johnson', 'qwerty789');
+
+INSERT INTO "user" (name, password, company, customer_code, type)
+VALUES
+  ('John Doe', 'password123', 'Company A', 12345, 'person'),
+  ('Jane Smith', 'securepass', 'Company B', 67890, 'company'),
+  ('Bob Johnson', 'pass1234', 'Company C', 54321, 'person'),
+  ('Alice Williams', 'mypassword', '', 98765, 'person'),
+  ('Charlie Brown', 'p@ssw0rd', 'Company B', 13579, 'company');
+
